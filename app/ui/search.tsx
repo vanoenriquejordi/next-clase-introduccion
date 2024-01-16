d3ff654c510edf55a0854f240961e386894dd10f
Jordi Vañó Enrique <vanoenriquejordi@gmail.com>
2024-01-16 03:10:57 +0100

    Summary

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,15 +1,17 @@
 'use client';
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
  
-export default function Search() {
-  // Inside the Search Component...
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+ 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-   
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -25,7 +27,6 @@ export default function Search() {
       </label>
       <input
   className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-  placeholder={placeholder}
   onChange={(e) => {
     handleSearch(e.target.value);
   }}
