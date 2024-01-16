7fd5e4b9ea4b9a43723800147c4df02e67ce652c
Jordi Vañó Enrique <vanoenriquejordi@gmail.com>
2024-01-16 02:55:46 +0100

    Update search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,22 +1,12 @@
 'use client';
-
+ 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
  
-export default function Search() {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
- 
+export default function Search({ placeholder }: { placeholder: string }) {
   function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
+    console.log(term);
   }
+ 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -28,7 +18,6 @@ export default function Search() {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
